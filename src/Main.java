0e92cd9f15c66699c3957b3ec9582c81810c85b5
Olivcampus <oliver.himblot@le-campus-numerique.fr>
2023-09-11 16:20:59 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    private class Warrior {
+        String Name = "name";
+        int life = 10;
+        int force = 10;
+        EquipementOffensif();
+        EquipementDefensif();
+
+
+    }
+    private class Personnage() {
+        String name;
+        int life;
+        int force;
+        int weapons;
+        int armors;
+
+    }
+    private class double EquipementOffensif(){
+
+       int weapon = 0;
+        return weapon;
+    }
+
+    private class double EquipementDefensif(){
+        int armor = 0;
+        return armor;
+    }
+
+    private class Menu{
+
+    }
+
+    private class Game{
+
+    }
+
+}
