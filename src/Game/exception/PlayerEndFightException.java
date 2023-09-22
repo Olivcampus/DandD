3d0a4c1f6a66a36fba09d678c34d52bc317e5a65
Olivcampus <oliver.himblot@le-campus-numerique.fr>
2023-09-22 15:48:53 +0200

    corriger bug event

--- src/Game/exception/PlayerEndFightException.java
+++
@@ -1,8 +0,0 @@
-package Game.exception;
-
-public class PlayerEndFightException extends Exception {
-    public PlayerEndFightException()   {
-        System.out.println("vous êtes mort");
-    }
-
-}
