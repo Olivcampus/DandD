eff0a35ae23952a30d23968b6623c47d7fc0c2b3
Olivcampus <oliver.himblot@le-campus-numerique.fr>
2023-09-28 14:09:59 +0200

    corrections mineures

--- src/Event/GenerateEvent/GenerateEventAttackMonster.java
+++ src/Event/GenerateEvent/GenerateEventAttackMonster.java
@@ -12,31 +12,32 @@ public class GenerateEventAttackMonster {
 
     public void fightMonster(Personnage player, Personnage enemy) {
 
-        if ((player.isAlive()) && enemy.isAlive())
-            System.out.println("vous allez affronter un " + enemy.getType() + " du nom de : " + enemy.getName() + " d'une force de : " + enemy.getForce() + " points et d'une santé de " + enemy.getLife() + "PV");
+        System.out.println("vous allez affronter un " + enemy.getType() + " du nom de : " + enemy.getName() + " d'une force de : " + enemy.getForce() + " points et d'une santé de " + enemy.getLife() + "PV");
         System.out.println("vous avez une force de : " + player.getForce() + " ,une vie de : " + player.getLife() + " et une résistance de " + player.getRightArm().getPowerArmor());
-        System.out.println("voulez vous : 1 :vous battre  2: fuir ?");
-        int choice = new InputScanner().intInputScanner();
-        if (choice == 1) {
-            enemy.setForce(enemy.getForce() - player.getRightArm().getPowerArmor());
-            int ramdomEvent = 1 + (int) (Math.random() * ((6 - 1) + 1));
-            if (ramdomEvent <= 2) {
-                System.out.println("l'ennemie à fui ");
-                return;
-            } else {
-                enemy.setLife(enemy.getLife() - player.getForce());
-                System.out.println("il vous reste " + player.getLife() + " PV et à votre ennemie " + enemy.getLife() + " PV");
+        if ((player.isAlive()) && enemy.isAlive()) {
+            System.out.println("voulez vous 1 : vous battre  2: fuir ?");
+            int choice = new InputScanner().intInputScanner();
+            if (choice == 1) {
+                enemy.setForce(enemy.getForce() - player.getRightArm().getPowerArmor());
+                int ramdomEvent = 1 + (int) (Math.random() * ((6 - 1) + 1));
+                if (ramdomEvent <= 2) {
+                    System.out.println("l'ennemie à fui ");
+                    return;
+                } else {
+                    enemy.setLife(enemy.getLife() - player.getForce());
+                    System.out.println("il vous reste " + player.getLife() + " PV et à votre ennemie " + enemy.getLife() + " PV");
+                }
+                if (enemy.isAlive()) {
+                    player.setLife(player.getLife() - enemy.getForce());
+                }
             }
-            if (enemy.isAlive()) {
-                player.setLife(player.getLife() - enemy.getForce());
-            }
-        }
-        if (choice == 2) {
-            System.out.println("vous fuyez");
-            int ramdomPosition = 1 + (int) (Math.random() * ((6 - 1) + 1));
-            player.setPlayerPosition(player.getPlayerPosition()- ramdomPosition) ;
-            if (player.getPlayerPosition() <= 0) {
-                player.setPlayerPosition(1);
+            if (choice == 2) {
+                int ramdomPosition = 1 + (int) (Math.random() * ((6 - 1) + 1));
+                player.setPlayerPosition(player.getPlayerPosition() - ramdomPosition);
+                if (player.getPlayerPosition() <= 0) {
+                    player.setPlayerPosition(1);
+                }
+                System.out.println("vous fuyez à la position : " + player.getPlayerPosition());
             }
         }
         if (!player.isAlive()) {
